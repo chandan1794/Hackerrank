b39ff9ef058cc5490b564ff3af6e17af6c1d6ecb
chandan1794 <kingpurbiaforever@gmail.com>
2016-06-26 09:26:01 -0700

    link
    https://www.hackerrank.com/challenges/finding-the-percentage

--- Python_First.py
+++ Python_First.py
@@ -1,3 +1,18 @@
-x = 3
-if x > 5
-print
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from __future__ import division
+N = int(raw_input())
+j = 0
+dict3 ={}
+marks = []
+while j<N:
+	name, x, y, z = raw_input().split(" ")
+	marks.extend([float(x),float(y),float(z)])
+	avg = sum(marks)/len(marks)
+	# print(marks)
+	# print(sum(marks))
+	# print(len(marks))
+	dict3[name] = avg
+	j = j+1
+	marks = [] 
+query = raw_input()
+print(format(dict3[query],'.2f'))
