93e5fc036902df91d6110e48d660790fcba85af3
chandan1794 <kingpurbiaforever@gmail.com>
2016-06-30 10:44:53 -0700

    https://www.hackerrank.com/challenges/python-tuples?h_r=next-challenge&h_v=zen

---
+++ tuples.py
@@ -0,0 +1,10 @@
+no_of_items = int(raw_input())
+
+items = []
+items = raw_input().split(" ")
+
+items = [int(i) for i in items] #comprehension
+
+print hash(items)
+items = tuple(items)
+print hash(items)
